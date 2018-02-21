41df9b9977d52c0ce2bcfb3e6a651ca7f0e29dae
im <maryna.todorishyna@ekreative.com>
2018-02-21 19:23:18 +0200

    Add react browser router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
+import {Router, Route} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
+
 import './App.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
     );
   }
 }
