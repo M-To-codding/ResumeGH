83f49b0c2a2592dc643f00c51aea99c1d3456fc1
im <maryna.todorishyna@ekreative.com>
2018-02-21 19:51:37 +0200

    Create component Resume

---
+++ src/Resume.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class Resume extends Component {
+  render() {
+    return (
+      <div className="resume">
+        Some text
+      </div>
+    );
+  }
+}
+
+export default Resume;
