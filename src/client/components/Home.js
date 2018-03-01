aea35c4ff4cc3d7c601d990a8af34604b4e5f4f6
im <maryna.todorishyna@ekreative.com>
2018-03-01 20:46:33 +0200

    Add css classes, change dom elements

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,54 +1,68 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import images from './../data/images';
 
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <header>
-          <div className="logo">
-          </div>
+        <header className="header">
+          <div className="container">
 
-          <nav>
-            <ul>
-              <li>
-                <a href="#" id="content-anchor" className="anchor">
-                  About
+            <nav className="nav-menu -header">
+              <div className="logo">
+                <a href="#">
+                  <img src={images.logo} alt=""/>
                 </a>
-              </li>
-              <li>
-                <a href="#" id="contact-anchor" className="anchor">
-                  Contact me
-                </a>
-              </li>
-            </ul>
-          </nav>
-
-          <div className="header-caption">
-            <div className="text-content">
-              <h1>
-                Lorem ipsum dolor sit amet
-              </h1>
-
-              <p>
-                Lorem ipsum dolor sit amet
-                Lorem ipsum dolor sit amet
-                Lorem ipsum dolor sit amet
-                Lorem ipsum dolor sit amet
-              </p>
-            </div>
-
-            <div className="photo-content">
-              <div className="photo">
-                <img src="" alt=""/>
               </div>
+
+              <ul className="menu-content">
+
+                <li className="nav-item">
+                  <a href="#" id="content-anchor" className="anchor">
+                    About
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a href="#" id="contact-anchor" className="anchor">
+                    Contact me
+                  </a>
+                </li>
+
+              </ul>
+            </nav>
+
+            <div className="header-caption">
+              <div className="text-content">
+
+                <h1 className="title">
+                  Lorem ipsum dolor sit amet
+                </h1>
+
+                <p className="subtitle">
+                  Lorem ipsum dolor sit amet
+                  Lorem ipsum dolor sit amet
+                  Lorem ipsum dolor sit amet
+                  Lorem ipsum dolor sit amet
+                </p>
+
+              </div>
+
+              <div className="photo-content">
+                <div className="photo">
+                  <img src={images.photo} alt=""/>
+                </div>
+              </div>
+
             </div>
           </div>
         </header>
 
-        <main>
+        <main className="main-info">
           <nav className="content-menu">
+
             <ul>
               <li>
                 <Link to="/skills">
@@ -61,11 +75,15 @@ class Home extends Component {
                 </Link>
               </li>
             </ul>
+
           </nav>
-          content
+
+          <section className="content">
+            content
+          </section>
         </main>
 
-        <footer>
+        <footer className="contacts">
           form
         </footer>
       </div>
