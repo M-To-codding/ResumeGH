055720055d84352124572a34d7e16769485e59ee
im <maryna.todorishyna@ekreative.com>
2018-03-03 21:36:50 +0200

    Refactor main component

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,28 +1,24 @@
 import React, {Component} from 'react';
-import {Router, Route} from 'react-router';
 import {BrowserRouter} from 'react-router-dom';
-import Skills from './Skills';
-import Resume from './Resume';
+import Header from './Header';
+import Footer from './Footer';
 
 
 import '../styles/App.css';
-import Home from './Home';
+import Content from './Content';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Home/>
 
-          <Route
-            path="/skills"
-            component={Skills}
-          />
-          <Route
-            path="/resume"
-            component={Resume}
-          />
+          <Header />
+
+          <Content />
+
+          <Footer />
+
         </div>
       </BrowserRouter>
     );
