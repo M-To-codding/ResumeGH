05cb7c976080dfdff4652c6cfb4ce92876df051b
im <maryna.todorishyna@ekreative.com>
2018-03-04 11:51:56 +0200

    Add form and form styles

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -7,8 +7,24 @@ class Footer extends Component {
 
     return (
       <footer className="contacts">
-        <div className="container">
-          form
+        <div className="container contacts-content">
+          <h5>If you want to contact me, fill and send contact form below</h5>
+          <form className="contact-form">
+
+            <div className="inputs">
+              <label htmlFor="name">
+                <input type="text" name="name" placeholder="Name" id="name"/>
+              </label>
+              <label htmlFor="email">
+                <input type="email" name="email" placeholder="Email" id="email"/>
+              </label>
+            </div>
+            <label htmlFor="message">
+              <textarea id="message" placeholder="Message"/>
+            </label>
+            <input type="submit" className="btn" value="Send"/>
+
+          </form>
         </div>
         <div className="copyright">
           ©copyright 2018
