281e040f2759157fc7c90cb2c506744b2d39dbee
im <maryna.todorishyna@ekreative.com>
2018-03-09 15:52:08 +0200

    Add redux routings store

--- src/client/components/Content.js
+++ src/client/components/Content.js
@@ -1,59 +1,73 @@
 import React, {Component} from 'react';
-import {Router, Route} from 'react-router';
+import {Route} from 'react-router';
 import {Link} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+
 import Skills from './Skills';
 import Resume from './Resume';
 import Title from './Title';
 import Greeting from './Greeting';
+import routing from './../reducers/routing';
 
 
 class Content extends Component {
   render() {
+
+    const store = createStore(routing);
+
     return (
-        <main className="main-info" id="content">
 
-          <Route component={Title} />
+     <Provider store={store}>
+        <BrowserRouter>
+          <main className="main-info" id="content">
 
-          <nav className="content-menu">
+            <Route component={Title}/>
 
-            <ul>
-              <li>
-                <Link to="/">
-                  Greeting
-                </Link>
-              </li>
-              <li>
-                <Link to="/skills">
-                  Skills
-                </Link>
-              </li>
-              <li>
-                <Link to="/resume">
-                  Resume
-                </Link>
-              </li>
-            </ul>
+            <nav className="content-menu">
 
-          </nav>
+              <ul>
+                <li>
+                  <Link to="/">
+                    Greeting
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/skills">
+                    Skills
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/resume">
+                    Resume
+                  </Link>
+                </li>
+              </ul>
 
-          <section className="content">
+            </nav>
 
-            <Route
-              exact
-              path="/"
-              component={Greeting}
-            />
-            <Route
-              path="/skills"
-              component={Skills}
-            />
-            <Route
-              path="/resume"
-              component={Resume}
-            />
+            <section className="content">
 
-          </section>
-        </main>
+              <Route
+                exact
+                path="/"
+                component={Greeting}
+              />
+              <Route
+                path="/skills"
+                component={Skills}
+              />
+              <Route
+                path="/resume"
+                component={Resume}
+              />
+
+            </section>
+          </main>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
