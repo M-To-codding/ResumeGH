5d9785c10e2a393eab92b691c932b5124295b050
im <maryna.todorishyna@ekreative.com>
2018-03-09 12:07:00 +0200

    Add form handler, fix styles

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -3,24 +3,48 @@ import React, {Component} from 'react';
 
 class Footer extends Component {
 
+  handleForm(event) {
+
+    event.preventDefault();
+
+    const data = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      msg: event.target.message.value
+    }
+    console.log(data);
+
+    fetch('/submit', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }, function () {
+      console.log(data);
+    })
+
+  }
+
   render() {
 
     return (
-      <footer className="contacts"  id="contact">
+      <footer className="contacts" id="contact">
         <div className="container contacts-content">
           <h5>If you want to contact me, fill and send contact form below</h5>
-          <form className="contact-form">
+          <form className="contact-form" onSubmit={this.handleForm}>
 
             <div className="inputs">
               <label htmlFor="name">
-                <input type="text" name="name" placeholder="Name" id="name"/>
+                <input type="text" name="name" placeholder="Name" id="name" required/>
               </label>
               <label htmlFor="email">
-                <input type="email" name="email" placeholder="Email" id="email"/>
+                <input type="email" name="email" placeholder="Email" id="email" required/>
               </label>
             </div>
             <label htmlFor="message">
-              <textarea id="message" placeholder="Message"/>
+              <textarea id="message" placeholder="Message" required/>
             </label>
             <input type="submit" className="btn" value="Send"/>
 
